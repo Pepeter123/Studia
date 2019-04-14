b671ca13fb5c0df8c53f0c7ba77cf3569b7e2b65
Pepeter <pepeter@hackerman.(none)>
2019-04-14 21:30:17 +0200

    instr5

--- PWiR/Instrukcja05/src/Exercise01/Test.java
+++ PWiR/Instrukcja05/src/Exercise01/Test.java
@@ -4,13 +4,18 @@ import Exercise01.data.Data;
 import Exercise01.delta.Delta;
 import Exercise01.roots.Roots;
 
+import java.util.concurrent.Semaphore;
+
 public class Test {
     private static Data data = new Data();
     private static Delta delta = new Delta(data);
+    private static Thread roots = new Thread(new Roots(data, delta));
+
+    static Semaphore semaphore = new Semaphore(1);
+
 
     public static void main(String[] args){
 
-        Thread roots = new Thread(new Roots(data, delta));
 
         data.run();
         delta.run();
--- PWiR/Instrukcja05/src/Exercise01/delta/Delta.java
+++ PWiR/Instrukcja05/src/Exercise01/delta/Delta.java
@@ -2,9 +2,13 @@ package Exercise01.delta;
 
 import Exercise01.data.Data;
 
+import java.util.concurrent.Semaphore;
+
 public class Delta implements Runnable {
     private double delta = 0.0;
     Data data = new Data();
+
+    Semaphore semaphore;
 
     public Delta(Data d) {
         this.data = d;
@@ -12,8 +16,18 @@ public class Delta implements Runnable {
 
     @Override
     public void run() {
-        delta = (Math.pow(data.getB(), 2) - 4 * data.getA() * data.getC());
-        System.out.println("Delta: " + delta);
+        try {
+            semaphore.acquire();
+
+            delta = (Math.pow(data.getB(), 2) - 4 * data.getA() * data.getC());
+            System.out.println("Delta: " + delta);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        semaphore.release();
+
     }
 
     public double getDelta() {
