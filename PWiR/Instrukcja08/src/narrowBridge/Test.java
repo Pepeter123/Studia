f7dee8e70dfcdd4b66b75edfd94b7e127ded8a99
Pepeter <pepeter@hackerman.(none)>
2019-05-27 00:13:08 +0200

    instr8 lights

--- PWiR/Instrukcja08/src/narrowBridge/Test.java
+++ PWiR/Instrukcja08/src/narrowBridge/Test.java
@@ -1,11 +1,15 @@
 package narrowBridge;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Semaphore;
 
 public class Test {
 
-    private final static int  WEST_QUEUE_SIZE = 10;
-    private final static int  EAST_QUEUE_SIZE = 10;
+    private final static int WEST_QUEUE_SIZE = 10;
+    private final static int EAST_QUEUE_SIZE = 10;
+
+    public static Semaphore semaphoreWest = new Semaphore(1);
+    public static Semaphore semaphoreEast = new Semaphore(0);
 
     public static void main(String[] args) throws Exception {
 
@@ -27,12 +31,22 @@ public class Test {
         Thread[] threadsEast = new Thread[EAST_QUEUE_SIZE];
 
 
-
         for (int i = 0, j = EAST_QUEUE_SIZE; i < EAST_QUEUE_SIZE; i++, j++) {
             threadsEast[i] = new Thread(westArray[i]);
             threadsWest[j] = new Thread(eastArray[j]);
-            threadsEast[i].start();
-            threadsWest[j].start();
+
+            if (semaphoreWest.availablePermits() == 1) {
+                threadsWest[j].start();
+                Thread.sleep(5000);
+                semaphoreWest.acquire();
+                semaphoreEast.release();
+            }
+            if (semaphoreEast.availablePermits() == 1) {
+                threadsEast[i].start();
+                Thread.sleep(5000);
+                semaphoreEast.acquire();
+                semaphoreWest.release();
+            }
         }
 
 
--- PWiR/Instrukcja08/src/narrowBridge/bridge/Bridge.java
+++ PWiR/Instrukcja08/src/narrowBridge/bridge/Bridge.java
@@ -4,8 +4,6 @@ import java.util.concurrent.Semaphore;
 
 public class Bridge {
 
-    public static Semaphore semaphoreWest = new Semaphore(1);
-    public static Semaphore semaphoreEast = new Semaphore(0);
 
 
 }
